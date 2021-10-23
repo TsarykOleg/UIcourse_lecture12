34ced02a25a014c75b2934d720cf58de81bdc681
TsarykOleg <tsaryk.oleg@gmail.com>
2021-10-24 01:59:45 +0300

    added rectangle.html, rectangle.js

---
+++ rectangle.js
@@ -0,0 +1,8 @@
+let a = prompt('Enter the side length', '1');
+let b = prompt('Enter the side width', '1');
+if (a != b) {alert('Perimeter of the rectangle: ' + (Number(a) + Number(b)) * 2)
+    } 
+    else {alert('Area of the square: ' + a * b)
+    
+}
+
