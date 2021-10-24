6232ae36fa1aa6b42d65695f71f0536d938a762d
TsarykOleg <tsaryk.oleg@gmail.com>
2021-10-24 21:27:34 +0300

    modified rectangle.js

--- rectangle.js
+++ rectangle.js
@@ -1,8 +1,17 @@
-let a = prompt('Enter the side length', '1');
-let b = prompt('Enter the side width', '1');
-if (a != b) {alert('Perimeter of the rectangle: ' + (Number(a) + Number(b)) * 2)
+// I didn't like it!!! 
+// let a = prompt('Enter the side length', '1');
+// let b = prompt('Enter the side width', '1');
+// if (a != b) {alert('Perimeter of the rectangle: ' + (Number(a) + Number(b)) * 2)
+//     } 
+//     else {alert('Area of the square: ' + a * b)
+    
+// }
+
+// I like it!!!
+let a = +prompt('Enter the side length', '1');
+let b = +prompt('Enter the side width', '1');
+if (a != b) {alert('Perimeter of the rectangle: ' + (a + b) * 2)
     } 
     else {alert('Area of the square: ' + a * b)
     
 }
-
